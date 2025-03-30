1496227c8655dafbb8618260efd04925154bdc6a
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2025-03-30 21:26:27 +0900

    Add alternatives

--- 2024/fsharp/2024-11.fsx
+++ 2024/fsharp/2024-11.fsx
@@ -22,6 +22,7 @@ let memo = Dictionary<int64, int64 list>()
 
 let checkStone (stone: int64) =
     if stone < 0 then failwith $"A negative value was passed in. This is bad."
+
     if memo.ContainsKey stone
     then
         memo.GetValueOrDefault stone
@@ -37,20 +38,40 @@ let checkStone (stone: int64) =
         memo.Add(stone, result)
         result
 
-// let combinedCollect times list =
-//     printfn $"Iterations: " // TODO: Delete after debugging.
-//     List.fold (fun acc i ->
-//         printf $"#{i} " // TODO: Delete after debugging.
-//         List.collect checkStone acc) list [1..times]
+// let checkStone (stone: int64) =
+//     if stone < 0 then failwith $"A negative value was passed in. This is bad."
+
+//     if memo.ContainsKey stone
+//     then
+//         memo.GetValueOrDefault stone
+//     else
+//         let result =
+//             match stone with
+//             | 0L -> List [1L]
+//             | s when isEven(digitCount s) ->
+//                 let halfway = digitCount s / 2
+//                 let l, r = Math.DivRem(s, pown 10 halfway)
+//                 List [l; r]
+//             | s -> List [s * 2024L]
+//         memo.Add(stone, result)
+//         result
 
 let combinedCollect times list =
-    let mutable current = list
-    for _ in 1 .. times do
-        let builder = System.Collections.Generic.List<_>()
-        for item in current do
-            builder.AddRange(checkStone item)
-        current <- List.ofSeq builder
-    current
+    printfn $"Iterations: " // TODO: Delete after debugging.
+    List.fold (fun acc i ->
+        printf $"#{i} " // TODO: Delete after debugging.
+        List.collect checkStone acc) list [1..times]
+
+// let combinedCollect times list =
+//     let mutable current = list
+//     for _ in 1 .. times do
+//         // Create a mutable builder to accumulate the next result.
+//         let builder = System.Collections.Generic.List<_>()
+//         for item in current do
+//             builder.AddRange(checkStone item)
+//         // Convert the builder (a sequence) back to an F# list.
+//         current <- List.ofSeq builder
+//     current
 
 let measureTime label f =
     let watch = Startwatch.Library.Watch()
@@ -58,4 +79,4 @@ let measureTime label f =
     printfn $"""%s{label}: %d{result} ({watch.ElapsedFriendly})"""
 
 measureTime "前" (fun _ -> input |> combinedCollect 25 |> _.Length)
-measureTime "後" (fun _ -> input |> combinedCollect 75 |> _.Length)
+// measureTime "後" (fun _ -> input |> combinedCollect 75 |> _.Length)
